815d49c849f151be1e0e726755bb074aef7d80d9
Abhinav <abhinavrawat728@gmail.com>
2020-07-11 16:55:00 +0530

    host-url changed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const db = mysql.createConnection({
     host:"http://3.135.205.171",
-    port:"80",
+    port:"3306",
     user:"root",
     password:"root",
     database:"stockmanager"
